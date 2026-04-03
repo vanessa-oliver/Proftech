31d1faa4212e14ae6b988beffb842e3e62215b21
vanessa <vanessa.n.d.oliveira@gmail.com>
2026-04-03 17:08:41 -0300

    ajustes no bd

--- config/database.js
+++ config/database.js
@@ -2,15 +2,10 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-    }
-);
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'database.sqlite'
+});
 sequelize.authenticate() 
     .then(() => { 
         console.log('Banco de dados conectado com sucesso'); 
